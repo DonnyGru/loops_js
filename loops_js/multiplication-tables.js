5eca25599cd85e063a81e05c3915254fded649d0
DonnyGru <91632259+DonnyGru@users.noreply.github.com>
2021-11-29 21:05:54 +0100

    Add files via upload

---
+++ loops_js/multiplication-tables.js
@@ -0,0 +1,13 @@
+const x = 9;
+for (i = 0; i < 10; i++){
+    const result = x * i;
+    console.log(i + "*" + x + "=" + result);
+}
+
+//Bonus
+for (y = 0; y <= 10; y++) {
+    for ( a = 0; a <= 10; a++) {
+      const result = y * a;
+      console.log(y + " * " + a + " = " + result);
+    }
+  }
